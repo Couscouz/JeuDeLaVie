4bf05eba3945486bc8218d716bacd15dccd57d1a
Lefada <61880638+Lefada@users.noreply.github.com>
2021-04-29 11:58:40 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -23,6 +23,11 @@ int** allouer(int taille);
 void initialiser(int** p_matrice, int lignes, int colonnes);
 
 
+
+void lire(int lignes, int colonnes, int tours, int torique, int** p_mat);
+
+
+
 /*-----------------------------------------------------------------*/
 /* Name : afficher                                                 */
 /* Auteur : Léo Portet                                             */
