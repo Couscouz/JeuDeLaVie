cadf56320571b8858281a3715f542f5d0d36d291
leo-create-rgb <83210317+leo-create-rgb@users.noreply.github.com>
2021-05-03 11:58:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "fonctions.h"
     
 int main() {
-
+  
     int lignes, colonnes;
     int tours, torique;  //matrice torique ou non
     int voisins;
@@ -11,28 +11,22 @@ int main() {
     int** p_matrice;
     int** p_temp;
 
-    scanf("%d",&colonnes);
-    scanf("%d",&lignes);
-    
-    printf("%d lignes et %d colo\n", lignes, colonnes);
-    
-    p_matrice = allouer(lignes, colonnes);
+    scanf("%d %d",&colonnes, &lignes); //lecture fichier texte nb lignes/colonnes
+
+    p_matrice = allouer(lignes, colonnes); //allocation mémoire lignes/colonnes
     p_temp = allouer(lignes, colonnes);
 
-    for(int i=0; i < lignes; i++){
+    /*Boucle de lecture du motif de départ dans le fichier texte*/
+    for(int i=0; i < lignes; i++){        
         for( int j=0;j<colonnes;j++){
             scanf("%d",&p_matrice[i][j]);
         }
     }  
 
-    scanf("%d",&tours);
-    scanf("%d",&torique);   
+    scanf("%d %d",&tours, &torique);  //lecture nb tours et si la matrice est torique ou non
     
     afficher(p_matrice, lignes, colonnes);
 
     return 0;
-
-    
-
-    
+     
 }
