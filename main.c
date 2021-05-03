70dff33904995d6d48949752dd264507a72460d4
leo-create-rgb <83210317+leo-create-rgb@users.noreply.github.com>
2021-05-03 11:48:56 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,67 +1,38 @@
 #include <stdlib>
 #include <stdio.h>
 #include "fonctions.h"
-
-
-
-int main{}{
+    
+int main() {
 
     int lignes, colonnes;
-
     int tours, torique;  //matrice torique ou non
-
     int voisins;
 
     int** p_matrice;
     int** p_temp;
 
+    scanf("%d",&colonnes);
+    scanf("%d",&lignes);
+    
+    printf("%d lignes et %d colo\n", lignes, colonnes);
+    
     p_matrice = allouer(lignes, colonnes);
     p_temp = allouer(lignes, colonnes);
 
-    initialiser(p_matrice, lignes, colonnes); //avec des 0
-    initialiser(p_temp, lignes, colonnes);
+    for(int i=0; i < lignes; i++){
+        for( int j=0;j<colonnes;j++){
+            scanf("%d",&p_matrice[i][j]);
+        }
+    }  
 
-
-    lire(lignes, colonnes, tours, torique, p_matrice);
-
-    printf("Il y a %d lignes et %d colonnes.\nIl y a %d tours et la matrice est torique=%d\n", lignes, colonnes, tours, torique);
+    scanf("%d",&tours);
+    scanf("%d",&torique);   
     
     afficher(p_matrice, lignes, colonnes);
 
     return 0;
 
-    //initialiser p_temp avec des 0
+    
 
-
-    for(int i=0; i<tours; i++){ //boucle principale
-
-        for(int m=0;m<lignes;m++){
-            for(int n=0;n<colonnes<;n++){
-
-                //on recense tous les voisins d'une case
-                //on calcule le nombre de chacun
-
-                //on calcule la valeur de la case qu'on stocke dans une matrice temporaire
-                p_temp[m][n] = etat(p_matrice, voisins, m, n);
-
-                
-            }
-        }
-        
-        //on remplace la matrice temporaire dans la principale
-        copieMatrice(p_temp, p_matrice, lignes, colonnes);
-
-        //on rafraichie le terminal
-        usleep(1000000);
-        system("clear");
-
-        //et on affiche notre nouvelle matrice
-        afficher(p_matrice, HAUTEUR, LARGEUR);
-
-    }
-    printf("Vous aez fait %d tours\n", tours);
-
-    liberer(p_matrice, LARGEUR)
-    liberer(p_temp, LARGEUR)
-    return 0;
+    
 }
