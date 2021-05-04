c7b24e6f3ee5a389155df7873cc9cfa37741135d
leo-create-rgb <83210317+leo-create-rgb@users.noreply.github.com>
2021-05-04 15:57:27 +0200

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -30,24 +30,26 @@ void initialiser(int** p_mat, int lignes, int colonnes){
 void afficher(int** p_matrice, int lignes, int colonnes){
     printf("\n");
     for(int i=0; i<lignes; i++) {
-        printf("===");
+        printf("=  ");
     }
+    /*---------------------Boucle d'affichage-------------------*/
     printf("\n");
     for(int i=0; i < lignes; i++) {
         for(int j=0; j < colonnes; j++) {
             printf("|");
             if(p_matrice[i][j] == 1) {
-                printf("\033[104m  \033[0m");
+                printf("\033[104m  \033[0m"); //remplissage des espaces avec la couleur bleu (cellule vivante)
             }
             else {
-                printf("\033[99m  \033[0m");
+                printf("  "); //pas de remplissage (pas de cellule)
             }
         }
         printf("\n");
 
     }
+    /*---------------------------------------------------------*/
     for(int i=0; i<lignes; i++) {
-        printf("===");
+        printf("=  ");
     }
     printf("\n");
 }
@@ -58,8 +60,8 @@ void afficher(int** p_matrice, int lignes, int colonnes){
 int voisins(int** p_mat, int ligne,int colonne, int lignes, int colonnes, int torique){
     int cpt = 0;
     
+    /*-------------------Si la matrice n'est pas Torique------------------*/
     if (!torique){   //matrice finie
-       
         for(int i=ligne-1;i<ligne+2;i++){
             for(int j=colonne-1;j<colonne+2;j++){
                 if (0<=i && i<lignes && 0<=j && j<colonnes && (i!=ligne || j!=colonne)){
@@ -69,6 +71,9 @@ int voisins(int** p_mat, int ligne,int colonne, int lignes, int colonnes, int to
         }
         
     }
+    /*-------------------------------------------------------------------*/
+    
+    /*--------------------Si la matrice est Torique----------------------*/
     else{
         int x,y;
         for(int i=ligne-1;i<ligne+2;i++){
@@ -92,6 +97,8 @@ int voisins(int** p_mat, int ligne,int colonne, int lignes, int colonnes, int to
             }
         }
     }
+    /*------------------------------------------------------------------*/
+    
     return cpt; //retourne le nombre de cellules voisines vivantes
 }
 
@@ -101,10 +108,12 @@ int voisins(int** p_mat, int ligne,int colonne, int lignes, int colonnes, int to
 /* Fonction qui permet de changer ou non l'état d'une cellule */
 int etat(int** p_mat, int nb_voisins, int x, int y){    
     int cel = p_mat[x][y];
-    if ((nb_voisins == 3 && cel == 0) ^ ((nb_voisins == 2 || nb_voisins == 3) && cel == 1)){ //naissance ou survie
-        /*Naissance de cellule*/
+    //naissance si une cellule vide a exactement 3 voisins | survie d'une cellule si elle a 2 ou 3 voisins
+    if ((nb_voisins == 3 && cel == 0) ^ ((nb_voisins == 2 || nb_voisins == 3) && cel == 1)){ 
+        /*Naissance d'une cellule*/
         return 1;
     }
+    //la cellule meurt d'isolement si elle a 1 voisin | la cellule meurt de surpopulation si elle a 4 voisins ou plus
     else{
         /*Mort d'une cellule*/
         return 0;
