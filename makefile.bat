6d189be8d09ac0b7867891b48c0e20330f3084f0
leo-create-rgb <83210317+leo-create-rgb@users.noreply.github.com>
2021-05-11 08:55:33 +0200

    Update makefile.bat

--- makefile.bat
+++ makefile.bat
@@ -1,3 +1,15 @@
-gcc -c fonctions.c -o fonctions.o
-gcc -c main.c -o fonctions.o
-gcc main.o fonctions.o -o JeuDeLaVie.exe
+exe : fonctions.o main.o
+  @echo "Compilation de l'exécutable"
+  gcc fonctions.o main.o -o exe
+ 
+ fonctions.o : fonctions.c fonctions.h
+   @echo "Compilation de fonctions"
+  gcc -c fonctions.c -o fonctions -o
+ 
+ main.o : main.c fonctions.h
+   @echo "Compilation du main"
+  gcc -c main.c -o fonctions.h
+  
+  clean :
+    @echo "On clean pour apporter les modifs"
+    rm -f* .o
