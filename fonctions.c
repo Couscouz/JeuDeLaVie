a4cf43de620440e6599ffb453326922a63becc0c
leo-create-rgb <83210317+leo-create-rgb@users.noreply.github.com>
2021-05-30 10:29:57 +0200

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -5,10 +5,10 @@
 
 /* Check */
 /* Fonction permettant d'allouer de la mémoire à la matrice */
-int** allouer(int lignes, int colonnes){
+int** allouer(int lignes, int colonnes) {
     int** p_tab;
     p_tab = malloc(lignes *sizeof(int*));
-    for(int i=0; i < lignes; i++){
+    for(int i=0; i < lignes; i++) {
         p_tab[i] = malloc(colonnes *sizeof(int));
     }
     return p_tab;
@@ -16,9 +16,9 @@ int** allouer(int lignes, int colonnes){
 
 /* Check */
 /* Procédure permettant d'initialiser les valeurs de la matrice */
-void initialiser(int** p_mat, int lignes, int colonnes){
-    for(int i=0; i < lignes; i++){
-        for(int j=0; j < colonnes; j++){
+void initialiser(int** p_mat, int lignes, int colonnes) {
+    for(int i=0; i < lignes; i++) {
+        for(int j=0; j < colonnes; j++) {
             p_mat[i][j] = 0;
         }
     }
@@ -26,7 +26,7 @@ void initialiser(int** p_mat, int lignes, int colonnes){
 
 /* Check */
 /* Procédure permettant d'afficher le matrice */
-void afficher(int** p_matrice, int lignes, int colonnes){
+void afficher(int** p_matrice, int lignes, int colonnes) {
     printf("\n");
     for(int i=0; i<colonnes; i++) {
         printf("=  ");
@@ -47,7 +47,7 @@ void afficher(int** p_matrice, int lignes, int colonnes){
 
     }
     /*---------------------------------------------------------*/
-    for(int i=0; i<colonnes; i++) {
+    for(int i=0; i < colonnes; i++) {
         printf("=  ");
     }
     printf("\n");
@@ -56,14 +56,14 @@ void afficher(int** p_matrice, int lignes, int colonnes){
 
 /* Check */
 /* Fonction qui permet de savoir le nombre de voisins de pour chaque cellule */ 
-int voisins(int** p_mat, int ligne,int colonne, int lignes, int colonnes, int torique){
+int voisins(int** p_mat, int ligne, int colonne, int lignes, int colonnes, int torique) {
     int cpt = 0;
     
     /*-------------------Si la matrice n'est pas Torique------------------*/
-    if (!torique){   //matrice finie
-        for(int i = ligne-1; i < ligne+2 ;i++){
-            for(int j = colonne-1; j < colonne+2; j++){
-                if (0 <= i && i < lignes && 0 <= j && j < colonnes && (i != ligne || j != colonne)){
+    if (!torique) {   //matrice finie
+        for(int i = ligne-1; i < ligne+2 ;i++) {
+            for(int j = colonne-1; j < colonne+2; j++) {
+                if (0 <= i && i < lignes && 0 <= j && j < colonnes && (i != ligne || j != colonne)) {
                     cpt += p_mat[i][j];
                 }
             }
@@ -72,15 +72,15 @@ int voisins(int** p_mat, int ligne,int colonne, int lignes, int colonnes, int to
     /*-------------------------------------------------------------------*/
     
     /*--------------------Si la matrice est Torique----------------------*/
-    else{
+    else {
         int x,y;
-        for(int i = ligne-1; i < ligne+2; i++){
-            for(int j = colonne-1; j < colonne+2; j++){
-                if (i != ligne || j != colonne){
-                    if (0 <= i && i < lignes && 0 <= j && j < colonnes){
+        for(int i = ligne-1; i < ligne+2; i++) { 
+            for(int j = colonne-1; j < colonne+2; j++) {
+                if (i != ligne || j != colonne) {
+                    if (0 <= i && i < lignes && 0 <= j && j < colonnes) { 
                         cpt += p_mat[i][j];
                     }
-                    else{
+                    else {
                         x = i;
                         y = j;
                         if (i == lignes){x = 0;}
@@ -104,15 +104,15 @@ int voisins(int** p_mat, int ligne,int colonne, int lignes, int colonnes, int to
 
 /* Check */
 /* Fonction qui permet de changer ou non l'état d'une cellule */
-int etat(int** p_mat, int nb_voisins, int x, int y){    
+int etat(int** p_mat, int nb_voisins, int x, int y) {    
     int cel = p_mat[x][y];
     //naissance si une cellule vide a exactement 3 voisins | survie d'une cellule si elle a 2 ou 3 voisins
-    if ((nb_voisins == 3 && cel == 0) ^ ((nb_voisins == 2 || nb_voisins == 3) && cel == 1)){ 
+    if ((nb_voisins == 3 && cel == 0) ^ ((nb_voisins == 2 || nb_voisins == 3) && cel == 1)) { 
         /*Naissance d'une cellule*/
         return 1;
     }
     //la cellule meurt d'isolement si elle a 1 voisin | la cellule meurt de surpopulation si elle a 4 voisins ou plus
-    else{
+    else {
         /*Mort d'une cellule*/
         return 0;
     }
@@ -120,16 +120,16 @@ int etat(int** p_mat, int nb_voisins, int x, int y){
 
 /* Check */
 /* Procédure permettant de modifier la matrice grâce une matrice temporaire */
-void copieMatrice(int** p_mat1, int** p_mat2, int lignes, int colonnes){   //la matrice 2 prends la valeur de la 1
-    for (int i=0; i<lignes; i++){
-        for (int j=0; j<colonnes; j++){
+void copieMatrice(int** p_mat1, int** p_mat2, int lignes, int colonnes) {   //la matrice 2 prends la valeur de la 1
+    for (int i=0; i < lignes; i++) {
+        for (int j=0; j < colonnes; j++) {
             p_mat2[i][j] = p_mat1[i][j];
         }
     }
 }
 
-void liberer(int** p_mat, int lignes){ //liberation de mémoire
-    for(int i=0; i<lignes; i++){
+void liberer(int** p_mat, int lignes) { //liberation de mémoire
+    for(int i=0; i < lignes; i++) {
         free(p_mat[i]);
     }
     free(p_mat);
