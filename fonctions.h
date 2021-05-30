5d0f911293d97c2a1c559f64e83623f8de979b4a
leo-create-rgb <83210317+leo-create-rgb@users.noreply.github.com>
2021-05-30 10:23:55 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -7,7 +7,7 @@
 
 /*-----------------------------------------------------------*/
 /* Name : allouer                                            */
-/* Auteur : Rémi Couzi                                       */
+/* Auteur : Ugo Latry                                        */
 /* Créée le : 27/04/2021                                     */
 /* Description : Fonction permettant d'allouer de la mémoire */
 /*-----------------------------------------------------------*/
@@ -16,7 +16,7 @@ int** allouer(int lignes, int colonnes);
 
 /*----------------------------------------------------------------------------*/
 /* Name : initialiser                                                         */
-/* Auteur : Rémi Couzi                                                        */
+/* Auteur : Anthony Cerisere                                                  */
 /* Créée le : 27/04/2021                                                      */
 /* Description : Procédure permettant d'initialiser les valeurs de la matrice */
 /*----------------------------------------------------------------------------*/
@@ -32,39 +32,39 @@ void initialiser(int** p_matrice, int lignes, int colonnes);
 void afficher(int** p_matrice, int lignes, int colonnes);
 
 
-/*-------------------------------------*/
-/* Name :  voisins*/
-/* Auteur :  Rémi Couzi & Léo Portet */
-/* Créée le : 4/05/2021 */
+/*-----------------------------------------------------------------------------------------*/
+/* Name :  voisins                                                                         */
+/* Auteur :  Rémi Couzi & Léo Portet                                                       */
+/* Créée le : 4/05/2021                                                                    */
 /* Description : Fonction qui permet de savoir le nombre de voisins de pour chaque cellule */
-/*-------------------------------------*/
-int voisins(int** p_mat, int ligne,int colonne, int lignes, int colonnes, int torique);
+/*-----------------------------------------------------------------------------------------*/
+int voisins(int** p_mat, int ligne, int colonne, int lignes, int colonnes, int torique);
 
  
-/*-------------------------------------*/
-/* Name :  etat */
-/* Auteur : Rémi Couzi & Léo Portet */
-/* Créée le : 03/05/2021 */
+/*--------------------------------------------------------------------------*/
+/* Name :  etat                                                             */
+/* Auteur : Rémi Couzi & Léo Portet                                         */
+/* Créée le : 03/05/2021                                                    */
 /* Description : Fonction qui permet de changer ou non l'état d'une cellule */
-/*-------------------------------------*/
+/*--------------------------------------------------------------------------*/
 int etat(int** p_mat, int nb_voisins, int y, int x);
 
 
-/*-------------------------------------*/
-/* Name :  copieMatrice */
-/* Auteur :  */ 
-/* Créée le : */
+/*----------------------------------------------------------------------------------------*/
+/* Name :  copieMatrice                                                                   */
+/* Auteur :  Ugo Latry                                                                    */ 
+/* Créée le : 4/05/2021                                                                   */
 /* Description : Procédure permettant de modifier la matrice grâce une matrice temporaire */
-/*-------------------------------------*/
+/*----------------------------------------------------------------------------------------*/
 void copieMatrice(int** p_mat1, int** p_mat2, int lignes, int colonnes);
 
 
-/*-------------------------------------*/
-/* Name :  */
-/* Auteur :  */ 
-/* Créée le : */
-/* Description : */
-/*-------------------------------------*/
+/*---------------------------------------------------------------*/
+/* Name :  liberer                                               */
+/* Auteur :  Anthony Cerisère                                    */ 
+/* Créée le : 27/04/2021                                         */
+/* Description : Procédure permettant de libérer l'espace mémoire*/
+/*---------------------------------------------------------------*/
 void liberer(int** p_mat, int lignes);
 
 #endif
